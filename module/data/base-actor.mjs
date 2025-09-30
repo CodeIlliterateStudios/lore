c507174710d35bbb2037d315eb2118d9bd8b2425
CodeIlliterateAdmin <birael@codeilliterate.com>
2025-09-29 20:37:18 -0400

    Refactor initial values for health, fatigue, and attributes in loreActorBase schema. Changed attribute modifier to equal value - 1.

--- module/data/base-actor.mjs
+++ module/data/base-actor.mjs
@@ -8,13 +8,13 @@ export default class loreActorBase extends foundry.abstract
     const schema = {};
 
     schema.health = new fields.SchemaField({
-      value: new fields.NumberField({...requiredInteger, initial: 10, min: 0, }),
-      max: new fields.NumberField({ ...requiredInteger, initial: 10 }),
+      value: new fields.NumberField({...requiredInteger, initial: 0, min: 0, }),
+      max: new fields.NumberField({ ...requiredInteger, initial: 3 }),
     });
 
     schema.fatigue = new fields.SchemaField({
-      value: new fields.NumberField({ ...requiredInteger, initial: 5, min: 0 }),
-      max: new fields.NumberField({ ...requiredInteger, initial: 5 }),
+      value: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
+      max: new fields.NumberField({ ...requiredInteger, initial: 3 }),
     });
 
     // Iterate over attribute names and create a new SchemaField for each.
@@ -23,8 +23,8 @@ export default class loreActorBase extends foundry.abstract
         obj[attribute] = new fields.SchemaField({
           value: new fields.NumberField({
             ...requiredInteger,
-            initial: 10,
-            min: 0,
+            initial: 1,
+            min: 1,
           }),
         });
         return obj;
@@ -38,10 +38,8 @@ export default class loreActorBase extends foundry.abstract
   prepareDerivedData() {
     // Loop through attribute scores, and add their modifiers to our sheet output.
     for (const key in this.attributes) {
-      // Calculate the modifier using d20 rules.
-      this.attributes[key].mod = Math.floor(
-        (this.attributes[key].value - 10) / 2
-      );
+      // Modifier is value - 1 (rank 1 = 0, rank 2 = 1, etc).
+      this.attributes[key].mod = (this.attributes[key].value - 1);
       // Handle attribute label localization.
       this.attributes[key].label =
         game.i18n.localize(CONFIG.LORE.attributes[key]) ?? key;
